a2e3122eac67cd4a410b73770f8523fa3efe0728
Andrew Neitsch <andrew@neitsch.ca>
2021-08-04 09:12:15 -0600

    Exclude undefined stems in convert/CW.js instead
    https://github.com/UAlbertaALTLab/crk-db/pull/79#discussion_r678009051

--- bin/buildDatabase.js
+++ bin/buildDatabase.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
 import buildDatabase from '../lib/buildDatabase.js';
-import program from "commander";
 
-program
-    .action(buildDatabase);
-
-program.parseAsync(process.argv);
+buildDatabase();
